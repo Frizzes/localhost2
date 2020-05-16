3cfbc3dfdc75b364256fa9f9715255d4bfbcf8bf
Frizzes <Frizzes-Art@ya.ru>
2020-05-16 15:35:27 +0300

    cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'Frizzes',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
